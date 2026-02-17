0dcf97a233340007179e3eaccd7d3066366edd47
devansh <devanshrastogi50@gmail.com>
2026-02-17 10:51:01 +0530

    UC4: Banner stored in array and printed using loop

---
+++ UC4.java
@@ -0,0 +1,27 @@
+
+public class UC4 {
+
+    static String gap = "   ";
+
+    // method to build a single banner line
+    static String buildLine(String... parts) {
+        return String.join(gap, parts);
+    }
+
+    public static void main(String[] args) {
+
+        
+        String[] banner = {
+                buildLine("OOOOO", "OOOOO", "PPPPP", "SSSSS"),
+                buildLine("O   O", "O   O", "P   P", "S"),
+                buildLine("O   O", "O   O", "PPPPP", "SSSSS"),
+                buildLine("O   O", "O   O", "P", "        S"),
+                buildLine("OOOOO", "OOOOO", "P","    SSSSS")
+        };
+
+        
+        for(String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
