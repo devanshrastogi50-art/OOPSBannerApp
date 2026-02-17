94398ad19f8f592fa0d400a2041d8d667997a75e
devansh <devanshrastogi50@gmail.com>
2026-02-17 10:27:47 +0530

    UC2: Added OOPS banner using println statements

---
+++ UC2.java
@@ -0,0 +1,14 @@
+public class UC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOOOO   OOOOO   PPPPP   SSSSS");
+        System.out.println("O   O   O   O   P   P   S");
+        System.out.println("O   O   O   O   PPPPP   SSSSS");
+        System.out.println("O   O   O   O   P           S");
+        System.out.println("OOOOO   OOOOO   P       SSSSS");
+
+    }
+}
+
+
